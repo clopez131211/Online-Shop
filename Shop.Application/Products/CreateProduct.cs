73a2a4935895f34973aa1aed40da66c79d59c5db
numberoneecsstudent <32531002+numberoneecsstudent@users.noreply.github.com>
2019-05-15 21:18:14 -0700

    Add project files.

---
+++ Shop.Application/Products/CreateProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Shop.Database;
+using Shop.Domain.Models;
+
+namespace Shop.Application.Products
+{
+    public class CreateProduct
+    {
+        private ApplicationDBContext _context;
+        public CreateProduct(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public void Do(int id, string Name, string Description)
+        {
+            _context.Products.Add(new Product { Id = id, Name = Name, Description = Description });
+        }
+    }
+}
